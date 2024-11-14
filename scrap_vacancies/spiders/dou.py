1a062100258223161ec3b5f23e13c79898b5059e
Misha Havryliuk <misharob31415@gmail.com>
2024-11-14 20:49:54 +0200

    analysis + readme

--- scrap_vacancies/spiders/dou.py
+++ scrap_vacancies/spiders/dou.py
@@ -1,18 +1,15 @@
 import re
-
 import scrapy
 import json
 import time
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from scrapy.http import HtmlResponse
+from scrapy.http import Response, HtmlResponse
 from scrap_vacancies.items import ScrapVacanciesItem
-
 
 class DouSpider(scrapy.Spider):
     name = "dou"
     allowed_domains = ["jobs.dou.ua"]
-
     start_urls = [
         "https://jobs.dou.ua/vacancies/?category=Python&exp=0-1",
         "https://jobs.dou.ua/vacancies/?category=Python&exp=1-3",
@@ -29,7 +26,7 @@ class DouSpider(scrapy.Spider):
         with open(file_path, "r") as file:
             return json.load(file)
 
-    def parse(self, response):
+    def parse(self, response: Response):
         self.driver.get(response.url)
 
         while True:
@@ -41,28 +38,31 @@ class DouSpider(scrapy.Spider):
                 break
 
         html = self.driver.page_source
-        scrapy_response = HtmlResponse(url=self.driver.current_url, body=html, encoding='utf-8')
+        scrapy_response = HtmlResponse(url=self.driver.current_url, body=html, encoding="utf-8")
 
         experience = self.get_experience_from_url(response.url)
 
         vacancies = scrapy_response.css("li.l-vacancy")
         for vacancy in vacancies:
-            time.sleep(1)
+            time.sleep(2)
             vacancy_url = vacancy.css("a.vt::attr(href)").get()
             if vacancy_url:
-                yield scrapy_response.follow(vacancy_url, callback=self.parse_vacancy, meta={'experience': experience})
+                yield scrapy_response.follow(vacancy_url, callback=self.parse_vacancy, meta={"experience": experience})
 
         self.driver.quit()
 
-    def parse_vacancy(self, response):
+    def parse_vacancy(self, response: Response) -> ScrapVacanciesItem:
         item = ScrapVacanciesItem()
 
         item["title"] = response.css("h1.g-h2::text").get()
         item["company"] = response.css("div.l-n a::text").get()
         item["location"] = response.css("span.place::text").get()
-        item["salary"] = response.css("span.salary::text").get()
-        if item["salary"]:
-            item["salary"] = re.sub(r"[^0-9$-]", "", item["salary"])
+
+        salary = response.css("span.salary::text").get()
+        if salary:
+            salary = re.sub(r"[^0-9\-$ ]", "", salary).strip()
+
+        item["salary"] = salary
 
         description = response.css("div.b-typo.vacancy-section").getall()
         item["description"] = " ".join(description).strip()
