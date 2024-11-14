bc9a4802a6384bae17979ec10bbf0357ce73f6c5
Misha Havryliuk <misharob31415@gmail.com>
2024-11-14 17:42:23 +0200

    add scraping

--- scrap_vacancies/spiders/dou.py
+++ scrap_vacancies/spiders/dou.py
@@ -1,6 +1,7 @@
+import re
+
 import scrapy
 import json
-import re
 import time
 from selenium import webdriver
 from selenium.webdriver.common.by import By
@@ -11,7 +12,13 @@ from scrap_vacancies.items import ScrapVacanciesItem
 class DouSpider(scrapy.Spider):
     name = "dou"
     allowed_domains = ["jobs.dou.ua"]
-    start_urls = ["https://jobs.dou.ua/vacancies/?category=Python"]
+
+    start_urls = [
+        "https://jobs.dou.ua/vacancies/?category=Python&exp=0-1",
+        "https://jobs.dou.ua/vacancies/?category=Python&exp=1-3",
+        "https://jobs.dou.ua/vacancies/?category=Python&exp=3-5",
+        "https://jobs.dou.ua/vacancies/?category=Python&exp=5plus"
+    ]
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
@@ -34,15 +41,16 @@ class DouSpider(scrapy.Spider):
                 break
 
         html = self.driver.page_source
-
         scrapy_response = HtmlResponse(url=self.driver.current_url, body=html, encoding='utf-8')
+
+        experience = self.get_experience_from_url(response.url)
 
         vacancies = scrapy_response.css("li.l-vacancy")
         for vacancy in vacancies:
             time.sleep(1)
             vacancy_url = vacancy.css("a.vt::attr(href)").get()
             if vacancy_url:
-                yield scrapy_response.follow(vacancy_url, callback=self.parse_vacancy)
+                yield scrapy_response.follow(vacancy_url, callback=self.parse_vacancy, meta={'experience': experience})
 
         self.driver.quit()
 
@@ -53,15 +61,16 @@ class DouSpider(scrapy.Spider):
         item["company"] = response.css("div.l-n a::text").get()
         item["location"] = response.css("span.place::text").get()
         item["salary"] = response.css("span.salary::text").get()
+        if item["salary"]:
+            item["salary"] = re.sub(r"[^0-9$-]", "", item["salary"])
 
         description = response.css("div.b-typo.vacancy-section").getall()
         item["description"] = " ".join(description).strip()
 
+        item["experience"] = response.meta.get('experience', 'N/A')
+
         technologies = self.extract_technologies(item["description"])
         item["technologies"] = technologies
-
-        experience = self.extract_experience(item["description"])
-        item["experience"] = experience
 
         yield item
 
@@ -71,6 +80,13 @@ class DouSpider(scrapy.Spider):
         ]
         return found_technologies
 
-    def extract_experience(self, description_text):
-        match = re.search(r"(\d+\+? years)", description_text)
-        return match.group(0) if match else "N/A"
+    def get_experience_from_url(self, url):
+        if "exp=0-1" in url:
+            return "0-1 years"
+        elif "exp=1-3" in url:
+            return "1-3 years"
+        elif "exp=3-5" in url:
+            return "3-5 years"
+        elif "exp=5plus" in url:
+            return "5+ years"
+        return "N/A"
